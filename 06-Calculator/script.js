c141556d423a9c518be36bf0d307eae6a0dd78e8
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-09-17 14:18:53 -0300

    06

---
+++ 06-Calculator/script.js
@@ -0,0 +1,18 @@
+// display function
+function display(val){
+    document.getElementById('result').value += val;
+    return val;
+}
+
+// main logic
+function solve(){
+    let x = document.getElementById('result').value;
+    let y = eval(x);
+    document.getElementById('result').value = y;
+    return y;
+}
+
+// Erase the screen
+function clearScreen(){
+    document.getElementById('result').value = '';
+}
---
+++ readme.txt
@@ -0,0 +1,5 @@
+05- Notes App from conding_dev_
+https://www.instagram.com/p/Cg6qP8drrCJ/
+
+06- Project Calculator from conding_dev_
+https://www.instagram.com/p/Cgtxg6drG1M/
