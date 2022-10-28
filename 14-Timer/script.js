163067bc09aa1f10c39c35b2965bef4a5e46c322
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-10-28 13:44:43 -0300

    14 edit

--- 14-Timer/script.js
+++ 14-Timer/script.js
@@ -1,23 +1,45 @@
-let interval;
-let timer = document.getElementById("userInput");
-let countDownSeconds;
+document.addEventListener("DOMContentLoaded", () => {
+    const $timekeeper = document.querySelector("#timekeeper"),
+          $startBtn = document.querySelector("#startBtn"),
+          $pauseBtn = document.querySelector("#pauseBtn"),
+          $stopBtn = document.querySelector("#stopBtn"),
+          $mins = document.querySelector("#mins"),
+          $secs = document.querySelector("#secs"),
+          $inputContainer = document.querySelector("#inputContainer");
+    
+    let idInterval = null,
+        timeDifference = 0,
+        dateTime = null;
 
-const startTime = () => {
-    interval = setInterval(start, 1000);
-    document.getElementById("displayTimer").innerHTML = timer.value;
-    interval;
-    countDownSeconds = timer.value;
-}
 
-const start = () => {
-    countDownSeconds--;
-    document.getElementById("displayTimer").innerHTML = countDownSeconds;
-    if(countDownSeconds === -1){
-        stopTime();
-        document.getElementById("displayTimer").innerHTML = "0";
-    }
-}
-
-const stopTime = () => {
-    clearInterval(interval);
-}
+    const startTimer = (mins, secs) => {
+        hideElement($inputContainer);
+        showElement($pauseBtn);
+        hideElement($startBtn);
+        hideElement($stopBtn);
+    
+        if(dateTime) {
+            dateTime = new Date();
+            let newDateTime = dateTime.getTime() + timeDifference;
+            console.log(newDateTime);
+            timeDifference = 0;
+        } else {
+            console.log('Start');
+            const milliseconds = (secs + (mins * 60)) * 1000;
+            newDateTime = new date(new Date().getTime() + milliseconds);
+        }
+    
+        clearInterval(idInterval);
+        idInterval = setInterval(() => {
+            const timekeeper = newDateTime.getTime() - new Date().getTime();
+            if(timekeeper <=0) {
+                console.log("Timer end");
+                clearInterval(idInterval);
+                hideElement($pauseBtn);
+                showElement($stopBtn);
+            } else {
+                $timekeeper.textContent = millisecondsToMinutesAndSeconds(timekeeper);
+            }
+        }, 50);
+    };
+});
