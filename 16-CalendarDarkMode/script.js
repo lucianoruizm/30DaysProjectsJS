c3bf6201f7a5f5599d4429d388144ec723d111af
Luciano Ruiz <lucianoruizm993@gmail.com>
2022-12-07 13:52:57 -0300

    16

--- 16-CalendarDarkMode/script.js
+++ 16-CalendarDarkMode/script.js
@@ -12,3 +12,38 @@ isLeapYear = (year) => {
         !==0) || (year % 100 === 0 && year % 400 === 0)
 }
 
+getFebDays = (year) => {
+    return isLeapYear(year) ? 29 : 28
+}
+
+let calendar = document.querySelector('.calendar')
+
+const month_names = ['January', 'February', 'March', 'April', 
+'May', 'June', 'July', 'August', 'September', 'October', 'November',
+'December']
+
+let month_picker = document.querySelectorAll('#month-picker')
+// GENERATE CALENDAR
+
+generateCalendar = (month, year) => {
+    let calendar_days = document.querySelector('.calendar')
+    let calendar_header_year = document.querySelector('#year')
+
+    let days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    let currDate = new Date()
+
+    month_picker.innerHTML = month_names[month]
+    calendar_header_year.innerHTML = year
+
+    let first_day = new Date(month, year, 1)
+
+    for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
+        let day = document.createElement('div')
+        if (i >= first_day.getDay()) {
+            day.classList.add('calendar-day-hover')
+            day.inenrHTML = i - first_day.getDay() + 1
+            day.innerHTML += ``
+        }
+    }
+}
